425753947eaa764397fdf159f5fd790793a09dcb
Nicolò Pincelli <pincelli.n@gmail.com>
2024-12-27 10:58:32 +0100

    create Library

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
     string ID { get; set; }
     int YearRegistration { get; set; }
 
-    string Denomination { get; }
+    public string Denomination { get; }
 }
 
 public class Person : IUser
@@ -15,5 +15,32 @@ public class Person : IUser
     public string Denomination()
     {
         return ID + Name + LastName;
+    }
+}
+public class Organization : IUser
+{
+    string? CompanyName { get; set; }
+    string? ID { get; set; }
+    int YearRegistration { get; set; }
+    public string Denomination()
+    {
+        return ID + CompanyName;
+    }
+}
+
+public class Library
+{
+    static Array Users;
+
+    static void List()
+    {
+        Console.WriteLine($"{Users}");
+
+    }
+    public void Main()
+    {
+        Persona a = new Persona("Nicolò", "Pincelli", 001);
+        Organization b = new Organization("Freelance", 001);
+
     }
 }
