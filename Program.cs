ef741a2b106bb6f98c72f4c0ffa7f55b78b39c78
Nicolò Pincelli <pincelli.n@gmail.com>
2024-12-27 11:02:49 +0100

    fix interface

--- Program.cs
+++ Program.cs
@@ -1,46 +1,51 @@
-﻿public interface IUser
+﻿
+namespace Biblio
 {
-    string ID { get; set; }
-    int YearRegistration { get; set; }
 
-    public string Denomination { get; }
-}
-
-public class Person : IUser
-{
-    string? Name { get; set; }
-    string? LastName { get; set; }
-    string? ID { get; set; }
-    int YearRegistration { get; set; }
-    public string Denomination()
+    public interface IUser
     {
-        return ID + Name + LastName;
+        string ID { get; set; }
+        int YearRegistration { get; set; }
+
+        string Denomination { get; }
     }
-}
-public class Organization : IUser
-{
-    string? CompanyName { get; set; }
-    string? ID { get; set; }
-    int YearRegistration { get; set; }
-    public string Denomination()
+
+    public class Person : IUser
     {
-        return ID + CompanyName;
+        string? Name { get; set; }
+        string? LastName { get; set; }
+        string? ID { get; set; }
+        int YearRegistration { get; set; }
+        public string Denomination()
+        {
+            return ID + Name + LastName;
+        }
     }
-}
-
-public class Library
-{
-    static Array Users;
-
-    static void List()
+    public class Organization : IUser
     {
-        Console.WriteLine($"{Users}");
-
+        string? CompanyName { get; set; }
+        string? ID { get; set; }
+        int YearRegistration { get; set; }
+        public string Denomination()
+        {
+            return ID + CompanyName;
+        }
     }
-    public void Main()
-    {
-        Persona a = new Persona("Nicolò", "Pincelli", 001);
-        Organization b = new Organization("Freelance", 001);
 
+    public class Library
+    {
+        static Array Users;
+
+        static void List()
+        {
+            Console.WriteLine($"{Users}");
+
+        }
+        public void Main()
+        {
+            Persona a = new Persona("Nicolò", "Pincelli", 001);
+            Organization b = new Organization("Freelance", 001);
+
+        }
     }
 }
