41244cd7642fe2df0b3dbe2358a2969737cb0a1b
Nicolò Pincelli <pincelli.n@gmail.com>
2024-12-27 10:43:55 +0100

    create class Person

--- Program.cs
+++ Program.cs
@@ -1,8 +1,19 @@
-﻿public interface IUtente
+﻿public interface IUser
 {
     string ID { get; set; }
     int YearRegistration { get; set; }
 
     string Denomination { get; }
+}
 
+public class Person : IUser
+{
+    string? Name { get; set; }
+    string? LastName { get; set; }
+    string? ID { get; set; }
+    int YearRegistration { get; set; }
+    public string Denomination()
+    {
+        return ID + Name + LastName;
+    }
 }
